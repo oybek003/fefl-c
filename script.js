208850be0b1dfc434e4ab2b10b7e27943e2720c1
oybek003 <ojbekmuslimov1@gmail.com>
2024-12-15 15:38:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+alert('sizda xozir random sonlar chiqadi');
+
+let realRandom = Math.floor (Math.random()*100)+1;
+alert(`sizda random orqali chiqan son: ${realRandom}`);
+
+// let first = ('5');
+// let second = ('10');
+// let result = (5 * 10)
+// alert(`${result}`);
+
+alert('biz random sonni 2 ga kopaytirmoqchi \n va bolmoqchimiz');
+
+let result2 = (2);
+let result3 = (`${realRandom}` * 2)
+console.log(`${result3}`);
+
+let squareRoot = Math.sqrt(2);
+let b = (`${realRandom}` / 2)
+console.log(`${b}`)
+
+alert(`
+    Random orqali chiqan son: ${realRandom} edi\n
+    biz uni 2 ga kopaytirib: ${result3} sonni chiqardik, \n
+    va sonni 2 ga bolib: ${b} sonni chiqardik.
+    `)
